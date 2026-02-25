01b1bc1af682632d501a083e91b03c3d43eaa798
Harold M <harold02139@gmail.com>
2026-02-25 18:52:20 -0500

    Cambios en la clase del 20 de febrero del 2026

--- servidor.py
+++ servidor.py
@@ -1,13 +1,40 @@
 import socket
+import threading
 
+contador_clientes = 0 #Recurso compartido
+lock = threading.Lock()
+
+def handle_client(conn, addr):
+    global contador_clientes
+    print(f"Cliente conectado desde {addr}")
+    try:
+        with lock:
+            contador_clientes += 1
+            numero = contador_clientes
+            
+        print(f"Cliente {numero} atendido desde {addr}")
+        student_name = conn.recv(1024).decode()
+        response = f"Hola {student_name} eres el cliente numero {numero}"
+        conn.sendall(response.encode())
+    except Exception as e:
+        print(f"Error con {addr}: {e}")
+    finally:
+        conn.close()
+        print(f"Conexion cerrada {addr}")
+#Crear server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 5000))
-server.listen(1)
+server.bind(("0.0.0.0", 5000))
+server.listen()
 
-print("Servidor esperando conexión...")
+print("Servidor concurrente escuchando...")
 
-conn, addr = server.accept()
-print("Cliente conectado:", addr)
+while True:
+    conn, addr = server.accept()
 
-conn.sendall(b"Harold Mauricio Melo Valencia")
-conn.close()
+    #Create a thread per cliente
+    #Crear Hilo por cliente
+    client_thread = threading.Thread(
+        target=handle_client,
+        args=(conn, addr)
+    )
+    client_thread.start()
